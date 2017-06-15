cc34da8a913d991c15be52d0dc2cfc76776fd221
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-06-15 18:38:07 +0200

    Make it workable without raspi

--- bierwiegen/gpio.py
+++ bierwiegen/gpio.py
@@ -1,26 +1,35 @@
-from RPi import GPIO
+import warnings
+try:
+    from RPi import GPIO
+    GPIO.setmode(GPIO.BOARD)
+    HAS_GPIO = True
+except RuntimeError:
+    HAS_GPIO = False
+    warnings.warn("Not on a raspberry pi")
+
 from threading import Thread, Event
-
-
-BUTTON_PIN = 11
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
 class ButtonWatchThread(Thread):
 
-    def __init__(self, widget):
+    def __init__(self, pin, widget):
         super().__init__()
+        self.button_pin = pin
         self.widget = widget
         self.event = Event()
 
+        if HAS_GPIO:
+            GPIO.setup(self.button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
     def run(self):
         while not self.event.is_set():
-            if(GPIO.input(BUTTON_PIN) == 1):
-                self.widget.button_press()
-                self.event.wait(0.2)
+            if HAS_GPIO:
+                if(GPIO.input(self.button_pin) == 1):
+                    self.widget.button_press()
+                    self.event.wait(0.2)
             self.event.wait(0.01)
 
     def terminate(self):
         self.event.set()
-        GPIO.cleanup()
+        if HAS_GPIO:
+            GPIO.cleanup()
